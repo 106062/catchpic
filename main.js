82f26f863519cd78094646e8a817741e94153200
Minato_KazeHaya <gcbotqe@gmail.com>
2021-10-19 12:03:41 +0800

    style: fix style

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require("fs");
 
-let  CreateTitle = require("./lib/Createmd.js").CreateTitle;
+const  CreateTitle = require("./lib/Createmd.js").CreateTitle;
 const PrintPage = require("./lib/Createmd.js").PrintPage;
 
 const config = require("./config/sample.json");
@@ -12,11 +12,11 @@ const end_page = config.end;
 function main() {
     try {
         let _file = "";
-        for (let i = 0; i < end_page;) {
-            let file = `${name}-${i + 1}-${i + 10}.md`;
+        for (let start = 0; i < end_page;) {
+            let file = `${name}-${start + 1}-${start + 10}.md`;
             _file = file;
-            CreateTitle(file, name, (i + 1), (i + 10));
-            for (let j = (i + 1); j <= (i + 10); j++) {
+            CreateTitle(file, name, (start + 1), (start + 10));
+            for (let j = (start + 1); j <= (start + 10); j++) {
                 PrintPage(_file, name, j);
             }
             i += 10;
